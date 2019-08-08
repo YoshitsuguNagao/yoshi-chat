9fd2e9fc14c115e941fbf24e6e93bd56539ed372
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-08-08 21:25:48 +0200

    emmiting massages

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var socket = require('socket.io');
+const express = require('express');
+const socket = require('socket.io');
 
 //App setup
-var app = express();
-var server = app.listen(4000, function() {
+const app = express();
+const server = app.listen(4000, function() {
   console.log('listening to reqests on port 4000');
 })
 
@@ -11,8 +11,12 @@ var server = app.listen(4000, function() {
 app.use(express.static('public'));
 
 // Socket setup
-var io = socket(server);
+const io = socket(server);
 
 io.on('connection', function(socket) {
-  console.log('made socket connection', socket.id)
-})
+  console.log('made socket connection', socket.id);
+
+  socket.on('chat', function(data) {
+    io.sockets.emit('chat', data);
+  });
+});
